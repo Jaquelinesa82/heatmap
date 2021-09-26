81659d955f35076d2fe390a95f3104c612b6eb25
jaqueline <jaquelinesa.82@gmail.com>
2021-09-26 17:07:14 -0300

    Inserido mapa no projeto com Folium

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Data(models.Model):
+    city = models.CharField(max_length=100, null=True)
+    population = models.PositiveIntegerField(null=True)
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
+
+
+    class Meta:
+        verbose_name_plural = 'Data'
+
+    def __str__(self):
+        return self.city
