60db887303a46568916390c23f6563f4ed023376
jaqueline <jaquelinesa.82@gmail.com>
2021-09-26 17:07:14 -0300

    Inserido mapa no projeto com Folium

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Data
 
-# Register your models here.
+
+@admin.register(Data)
+class DataAdmin(admin.ModelAdmin):
+    list_display = ['id', 'city', 'population']
+
+
+
+
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,15 @@
+import folium
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'dashboard/index.html')
+    map1 = folium.Map(location=[-15.793889, -47.882778], zoom_start=11)
+    folium.Marker(location=[-15.7589665, -47.879422],
+                  popup='Esplanada dos Ministerios',
+                  icon=folium.Icon(color='red', icon='info-sign')
+                  ).add_to(map1)
+    map1 = map1._repr_html_()
+    context = {
+            'map1': map1
+    }
+    return render(request, 'dashboard/index.html', context)
