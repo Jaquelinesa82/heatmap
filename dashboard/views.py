eaa2e9883a33874a116bf8f6b01e3f680791c5b9
jaqueline <jaquelinesa.82@gmail.com>
2021-09-30 16:30:16 -0300

    Inserido data no formulario

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,18 +9,19 @@ def index(request):
     data = Data.objects.all()
     data_list = Data.objects.values_list('latitude', 'longitude', 'population')
 
-    map1 = folium.Map(location=[-23, -50], zoom_start=5)
+    map1 = folium.Map(location=[-18, -50], zoom_start=5)
 
     plugins.HeatMap(data_list).add_to(map1)
     plugins.Fullscreen(position='topright').add_to(map1)
 
     map1 = map1._repr_html_()
     context = {
-        'map1': map1
+        'map1': map1,
+        'data_list': data
     }
-    return render(request, 'dashboard/index.html', context)
+    return render(request, 'dashboard/index.html', context=context)
 
 
 def list_all_data(request):
     data = Data.objects.filter(active=True)
-    return render(request, 'index.html', {'data': data})
+    return render(request, 'dashboard/index.html', {'data': data})
