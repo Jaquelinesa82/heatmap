6f090beeb7028a1400085ef7c90bd6b874c5cadf
jaqueline <jaquelinesa.82@gmail.com>
2021-10-02 15:59:10 -0300

    Criado o teste com django assetions e o crud(create)

--- dashboard/tests/test_index.py
+++ dashboard/tests/test_index.py
@@ -1,6 +1,21 @@
-from django.test import Client
+from django.urls import reverse
+from mapproject.django_assertions import assert_contains
+import pytest
 
 
-def test_status_code(client: Client):
-    resp = Client.get('/')
+@pytest.fixture
+def resp(client, db):
+    resp = client.get(reverse('dashboard:listData'))
+    return resp
+
+
+def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Heatmap</title>')
+
+
+def test_listData_link(resp):
+    assert_contains(resp, f'href="{reverse("dashboard:listData")}"><h2>Dados</h2></a>')
---
+++ mapproject/django_assertions.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+
+_test_case_ = TestCase()
+
+assert_contains = _test_case_.assertContains
+assert_not_contains = _test_case_.assertNotContains
