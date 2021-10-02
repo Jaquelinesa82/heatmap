e37e65492f6e6a10cf1f8d5119a17db537b5cee9
jaqueline <jaquelinesa.82@gmail.com>
2021-10-02 15:57:51 -0300

    Ajuste do template Dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, redirect
+from .forms import DataForm
 from .models import Data
 import folium
 from folium import plugins
-import geocoder
-# Create your views here.
 
 
-def index(request):
+def listData(request):
     data = Data.objects.all()
     data_list = Data.objects.values_list('latitude', 'longitude', 'population')
 
-    map1 = folium.Map(location=[-18, -50], zoom_start=5)
+    map1 = folium.Map(location=[-18, -50], zoom_start=2)
 
     plugins.HeatMap(data_list).add_to(map1)
     plugins.Fullscreen(position='topright').add_to(map1)
@@ -23,19 +22,10 @@ def index(request):
     return render(request, 'dashboard/index.html', context=context)
 
 
-def list_all_data(request):
-    data = Data.objects.filter(active=True)
-    return render(request, 'dashboard/index.html', {'data': data})
+def createData(request):
+    form = DataForm(request.POST or None)
 
-
-def register_data(request):
-    return render(request, 'dashboard/register.html')
-
-
-def register_submit(request):
-    city = request.POST.get('city')
-    population = request.POST.get('population')
-    latitude = request.GET.get('latitude')
-    longitude = request.GET.get('longitude')
-    data = Data.objects.create(city=city, population=population, latitude=latitude, longitude=longitude)
-    return redirect('/')
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request, 'dashboard/dataForm.html', {'form': form})
