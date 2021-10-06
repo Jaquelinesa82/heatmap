115d1bdfd1937a42ef91db672c6ce1756a5848fc
jaqueline <jaquelinesa.82@gmail.com>
2021-10-06 02:03:11 -0300

    Criado CRUD (create, read, update, delete)

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,27 +1,14 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from .forms import DataForm
 from .models import Data
 import folium
 from folium import plugins
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def listData(request):
     data = Data.objects.all()
     data_list = Data.objects.values_list('latitude', 'longitude', 'population')
-
-    parametro_page = request.GET.get('page', '1')
-    parametro_limit = request.GET.get('limit', '5')
-
-
-    if (not parametro_limit.isdigit( ) and int(parametro_limit)):
-        parametro_limit = '10'
-
-    data_paginador = Paginator(data, parametro_limit)
-    try:
-        page = data_paginador.page(parametro_page)
-    except (EmptyPage, PageNotAnInteger):
-        page = data_paginador.page('')
 
     map1 = folium.Map(location=[-18, -50], zoom_start=2)
 
@@ -33,17 +20,30 @@ def listData(request):
     context = {
         'map1': map1,
         'data_list': data,
-        'page': page
     }
     return render(request, 'dashboard/index.html', context)
 
 
-def createData(request):
+def create(request):
     form = DataForm(request.POST or None)
 
     if form.is_valid():
         form.save()
         return redirect('/')
-    return render(request, 'dashboard/dataForm.html', {'form': form})
+    return render(request, 'dashboard/create.html', {'form': form})
 
 
+def update(request, id):
+    data = Data.objects.get(id=id)
+    form = DataForm(request.POST or None, instance=data)
+
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('listData')
+    return render(request, 'dashboard/update.html', {'data': data})
+
+
+def delete(request, id):
+    data = Data.objects.get(id=id)
+    data.delete()
+    return HttpResponseRedirect('listData')
